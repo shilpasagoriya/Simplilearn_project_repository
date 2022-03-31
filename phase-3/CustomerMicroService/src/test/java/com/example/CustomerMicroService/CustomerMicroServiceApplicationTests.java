426533365815c35193f536e312ef4ddcc7f997fd
shilpasagoriya <91321682+shilpasagoriya@users.noreply.github.com>
2022-03-31 12:27:44 +0530

    Add files via upload

---
+++ phase-3/CustomerMicroService/src/test/java/com/example/CustomerMicroService/CustomerMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomerMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
