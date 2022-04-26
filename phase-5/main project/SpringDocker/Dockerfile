2eca8bc944b47e9f898b29752da602393520ce3c
shilpasagoriya <91321682+shilpasagoriya@users.noreply.github.com>
2022-04-26 13:06:49 +0530

    Add files via upload

---
+++ phase-5/main project/SpringDocker/Dockerfile
@@ -0,0 +1,4 @@
+From openjdk:8
+Expose 8080
+Add target/SpringDocker-0.0.1-SNAPSHOT.war SpringDocker-0.0.1-SNAPSHOT.war
+ENTRYPOINT ["java", "-jar", "/SpringDocker-0.0.1-SNAPSHOT.war"]
---
+++ phase-5/main project/SpringDocker/src/main/java/com/bhaiti/server/main/DockerController.java
@@ -0,0 +1,16 @@
+package com.bhaiti.server.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DockerController {
+	
+	@ResponseBody
+	@RequestMapping("/user")
+	public String display() {
+		return "hi user";
+	}
+
+}
